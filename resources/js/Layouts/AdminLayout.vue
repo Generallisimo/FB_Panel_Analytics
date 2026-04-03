cf9a2a4db0b979ee97d237ef910c1f23ecf8297b
Fsociety <kingfsocietyall@gmail.com>
2026-04-03 20:29:32 +0200

    add AdminLayout

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,129 @@
+<template>
+    <div class="app-wrapper">
+        <!--begin::Header-->
+        <nav class="app-header navbar navbar-expand navbar-dark">
+            <!--begin::Container-->
+            <div class="container-fluid">
+                <!--begin::Start Navbar Links-->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+                            <i class="bi bi-list"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item d-none d-md-block">
+                        <a href="#" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item d-none d-md-block">
+                        <a href="#" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+                <!--end::Start Navbar Links-->
+
+                <!--begin::End Navbar Links-->
+                <ul class="navbar-nav ms-auto">
+
+                    <!--begin::User Menu Dropdown-->
+                    <li class="nav-item dropdown user-menu">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <img src="/assets/img/user2-160x160.jpg" class="user-image rounded-circle shadow"
+                                alt="User Image" />
+                            <span class="d-none d-md-inline">Alexander Pierce</span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
+                            <!--begin::User Image-->
+                            <li class="user-header text-bg-primary">
+                                <img src="/assets/img/user2-160x160.jpg" class="rounded-circle shadow mx-auto"
+                                    alt="User Image" />
+                                <p>
+                                    Alexander Pierce
+                                </p>
+                            </li>
+                            <!--end::User Image-->
+                            <!--begin::Menu Footer-->
+                            <li class="user-footer text-center">
+                                <!-- <a href="#" class="btn btn-outline-secondary">Profile</a> -->
+                                <a href="#" class="btn btn-outline-danger ">Sign out</a>
+                            </li>
+                            <!--end::Menu Footer-->
+                        </ul>
+                    </li>
+                    <!--end::User Menu Dropdown-->
+                </ul>
+                <!--end::End Navbar Links-->
+            </div>
+            <!--end::Container-->
+        </nav>
+        <!--end::Header-->
+        <!--begin::Sidebar-->
+        <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+            <!--begin::Sidebar Brand-->
+            <div class="sidebar-brand">
+                <!--begin::Brand Link-->
+                <a href="./index.html" class="brand-link">
+                    <!--begin::Brand Image-->
+                    <img src="/assets/img/AdminLTELogo.png" alt="AdminLTE Logo"
+                        class="brand-image opacity-75 shadow" />
+                    <!--end::Brand Image-->
+                    <!--begin::Brand Text-->
+                    <span class="brand-text fw-light">AdminLTE 4</span>
+                    <!--end::Brand Text-->
+                </a>
+                <!--end::Brand Link-->
+            </div>
+            <!--end::Sidebar Brand-->
+            <!--begin::Sidebar Wrapper-->
+            <div class="sidebar-wrapper">
+                <nav class="mt-2">
+                    <!--begin::Sidebar Menu-->
+                    <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="navigation"
+                        aria-label="Main navigation" data-accordion="false" id="navigation">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                </svg>
+                                <p>Пользователи</p>
+                            </a>
+                        </li>
+                    </ul>
+                    <!--end::Sidebar Menu-->
+                </nav>
+            </div>
+            <!--end::Sidebar Wrapper-->
+        </aside>
+        <!--end::Sidebar-->
+
+
+
+
+
+
+
+
+
+
+            <!-- <aside class="min-h-screen bg-slate-950 w-1/4">
+                <Link :href="route('admin.posts.index')" class="block p-5 text-lg border-b">
+                Posts
+                </Link>
+            </aside> -->
+
+
+        <!--begin::App Main-->
+        <main class="app-main">
+                <slot />
+        </main>
+        <!--end::App Main-->
+    </div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/vue3';
+
+export default {
+        components:{
+        Link
+    },
+}
+</script>
